6f7b8b711f465813360c9c0ad12a80d39ebf7c87
toojessy <jnkamienski@my.waketech.edu>
2025-10-22 09:10:35 -0400

    Create CoffeeShop.java

---
+++ CoffeeShop.java
@@ -0,0 +1,13 @@
+public class CoffeeShop {
+    // TODO 1: Change the class name to CoffeeShop
+
+    public static void main(String[] args) {
+        // TODO 2: Create three CoffeeOrder objects
+        // Use a different constructor each time
+
+        // TODO 3: Call describeOrder() on each and print the result
+
+        // TODO 4: Print the total number of orders using
+        // CoffeeOrder.getTotalOrders()
+    }
+}
